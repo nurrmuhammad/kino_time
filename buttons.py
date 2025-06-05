719b4b2aae93083fddde3a2c5d2f37eee056fe6c
Nurmuhammad <nurmuhammadov.nurik@gmail.com>
2025-06-05 16:34:54 +0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,8 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-check = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text="✅ Obuna bo‘lish", url="https://t.me/nur02_17")],
-        [InlineKeyboardButton(text="🔄 Tekshirish", callback_data="Tekshirish")]
-    ]
-)
+check = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="✅ Obuna bo‘lish", url="https://t.me/nur02_17")],
+    [InlineKeyboardButton(text="🔄 Tekshirish", callback_data="Tekshirish")]
+])
